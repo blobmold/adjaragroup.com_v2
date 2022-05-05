a268afb6b31cf18f2d11e5da5134c23de5b853d8
George Paresishvili <g.paresishvili@outook.com>
2022-05-05 15:31:59 +0400

    added career categories

--- controllers/careers.js
+++ controllers/careers.js
@@ -1,11 +1,27 @@
 import Career from "../models/Career.js";
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default async (req, res) => {
-  let careers = await Career.find({});
+  try {
+    let careers = await Career.find(req.query);
 
-  if (careers.length === 0) careers = `No jobs available`;
+    let careerCategories = await fs.readFile(path.resolve(__dirname, "../config/careerCategories.json"));
 
-  res.render("careers", {
-    careers,
-  });
+    res.render("careers", {
+      careers,
+      careerCategories: JSON.parse(careerCategories),
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.render("404", {
+      message: error.message,
+      error,
+    });
+  }
 };
