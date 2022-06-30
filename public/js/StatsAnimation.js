30b0638e2f1d482d06361e1008990e806fa4e2d8
George Paresishvili <g.paresishvili@outlook.com>
2022-07-01 01:57:09 +0400

    fixed stat animation - now animates when observed

--- public/js/StatsAnimation.js
+++ public/js/StatsAnimation.js
@@ -1,24 +1,49 @@
-let stats = document.querySelectorAll(".stat-amount");
-let step = 1;
-let animationDuration = 2000;
-let start = Date.now();
+let stats = document.querySelectorAll(".stat-number");
+let statsSection = document.querySelector(".company-stats");
 
-for (let stat of stats) {
-  let amount = stat.dataset.amount;
-  let currentCount = 0;
+if (stats && statsSection) {
+  if ("IntersectionObserver" in window) {
+    let callback = (entries, observer) => {
+      for (let entry of entries) {
+        entry;
+        if (entry.isIntersecting) {
+          console.log("intersected");
+          statAnimation(stats);
+          observer.unobserve(statsSection);
+        }
+      }
+    };
 
-  let timer = setInterval(function () {
-    let timePassed = Date.now() - start;
+    let options = {
+      threshold: 0.5,
+    };
 
-    if (timePassed >= animationDuration) {
-      clearInterval(timer);
-      // Textcontent is always ending on the amount given in data-amount
-      stat.textContent = amount;
-      return;
-    }
+    let statsObserver = new IntersectionObserver(callback, options);
+    statsObserver.observe(statsSection);
+  }
+}
 
-    currentCount = timePassed * (amount / animationDuration);
-    stat.textContent = Math.round(currentCount);
+function statAnimation(stats) {
+  let step = 1;
+  let animationDuration = 2000;
+  let start = Date.now();
 
-  }, step);
+  for (let stat of stats) {
+    let amount = stat.dataset.amount;
+    let currentCount = 0;
+
+    let timer = setInterval(() => {
+      let timePassed = Date.now() - start;
+
+      if (timePassed >= animationDuration) {
+        clearInterval(timer);
+        // Textcontent is always ending on the amount given in data-amount
+        stat.textContent = amount;
+        return;
+      }
+
+      currentCount = timePassed * (amount / animationDuration);
+      stat.textContent = Math.round(currentCount);
+    }, step);
+  }
 }
